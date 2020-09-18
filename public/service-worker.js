5e3859cfb4d1e4d757302ece24228e65d3cb3b12
larry laconi <soverylarry@gmail.com>
2020-09-17 23:19:19 -0400

    adding updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,49 +1,85 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/favorites.html",
-  "/topic.html",
-  "/assets/css/style.css",
-  "/dist/app.bundle.js",
-  "/dist/favorites.bundle.js",
-  "/dist/topic.bundle.js"
-];
-
-
-const PRECACHE = "precache-v1";
-const RUNTIME = "runtime";
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
-  );
-});
-
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener("activate", event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.url.startsWith(self.location.origin)) {
+  "/styles.css",
+  "/index.js",
+  "/db.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/images/ledger.jpg",
+  ];
+  
+  const CACHE_MONEY = "static-cache-original";
+  const DATA_MONEY_NEW = "data-cache-new";
+  
+  // install
+  self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_MONEY).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+      .catch(function(err){
+        console.log("not running", err);
+      })
+    );
+    self.skipWaiting();
+  });
+  
+  // activate  
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_MONEY && key !== DATA_MONEY_NEW) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+    self.clients.claim();
+  });
+    
+  // fetch
+  self.addEventListener("fetch", event => {
+    // non GET requests are not cached and requests to other origins are not cached
+    if (
+      event.request.method !== "GET" ||
+      !event.request.url.startsWith(self.location.origin)
+    ) {
+      event.respondWith(fetch(event.request));
+      return;
+    }
+  
+    // handle runtime GET requests for data from /api routes
+    if (event.request.url.includes("/api/transaction")) {
+      // make network request and fallback to cache if network request fails (offline)
+      event.respondWith(
+        caches.open(DATA_MONEY_NEW).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              cache.put(event.request, response.clone());
+              return response;
+            })
+            .catch(() => caches.match(event.request));
+        })
+      );
+      return;
+    }
+  
+    // use cache first for all other requests for performance
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
         if (cachedResponse) {
           return cachedResponse;
         }
-
-        return caches.open(RUNTIME).then(cache => {
+  
+        // request is not in cache. make network request and cache the response
+        return caches.open(DATA_MONEY_NEW).then(cache => {
           return fetch(event.request).then(response => {
             return cache.put(event.request, response.clone()).then(() => {
               return response;
@@ -52,5 +88,4 @@ self.addEventListener("fetch", event => {
         });
       })
     );
-  }
-});
+  });
